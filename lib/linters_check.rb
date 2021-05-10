65baba20e5f5868d43f7dd2b0a8e3170924f42d1
Aleksandar Ivezic <a.ivezic@yahoo.com>
2021-05-10 12:46:48 +0200

    Add check_indentation method

--- lib/linters_check.rb
+++ lib/linters_check.rb
@@ -1,16 +1,53 @@
 class Linter_check
-    attr_reader :file, :buffer, :lines
+    attr_reader :file, :buffer, :lines, :errors
     def initialize(path)
         @path = path
         @lines = IO.readlines(@path)
+        @errors = []
         run_check     
     end
 
     def run_check 
+        
+    end
+
+    def check_indentation
+        @ind = 0
+        @lines.each_with_index do |line, i|
+            @line_ind= 0
+            line_arr = line.split("")
+            line_arr.each do |x|
+                break if x!= " "
+                @line_ind += 1
+            end   
+            if line[i-1] && line[i-1].match("{\n")
+                @ind +=2
+            elsif line[i-1] && line[i-1].match("}")
+                @ind -=2
+            end
+            if @ind != @line_ind
+            @errors << "Indentation error on line #{i+1}"
+            end
+        end
+    end
+
+    def check_space_trailing
 
     end
+
+    def check_new_lines
+
+    end
+
+    def check_tags
+
+    end
+
 end   
 
 
 varbla = Linter_check.new("js_test.js")
-p varbla.lines[0]
+# buffer = StringScanner.new(varbla.lines[0])
+# p buffer
+varbla.check_indentation
+p varbla.errors
